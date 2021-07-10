ee9f8cc2b98b337d308190f9429eab26ebb27922
harshagowda15 <85866332+harshagowda15@users.noreply.github.com>
2021-07-10 16:26:05 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,12 @@
+n=int(input("enter the number of rows:"))
+def hpyramid(n):
+
+    for i in range(0, n):
+     
+        for j in range(0, i+1):
+         
+            print("* ",end="")
+      
+        print("\r")
+
+hpyramid(n)
